3f71e8b197264e639d1c3878fc7fc6b2c420479b
Tom Tamen <tomtamen@gmx.fr>
2022-09-24 15:14:13 +0200

    💫 add animations

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 /* max-h/w = 6 
 top = 8-920
 right =8-1908
+
+animation: move4 2s infinite;
 */
+
+function randomL(items){
+return items[Math.floor(Math.random()*items.length)];
+}
 
 function randomN(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-let nb = (window.innerHeight+window.innerWidth)/3;
+function randomF(min, max, decimals) {
+    return (Math.random() * (max - min) + min).toFixed(decimals);
+}
+
+
+
+let nb = (window.innerHeight+window.innerWidth)/2.5;
+const anims = ["move1","move2","move3","move4","move5","move6","move7","move8"];
 
 for(let i=0; i<=nb; i++){
     let star = document.createElement('div');
@@ -17,5 +30,7 @@ for(let i=0; i<=nb; i++){
     star.style.height = size+"px";
     star.style.top = randomN(0,window.innerHeight)+"px";
     star.style.right = randomN(0,window.innerWidth)+"px";
+    star.style.animation = randomL(anims)+" "+randomF(2,4,2)+"s "+"infinite ease-in-out";
+
     document.body.appendChild(star);
 }
